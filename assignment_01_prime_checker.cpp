d46a1a7ce23c9c20f5ea7fc859e158347bf203da
pamela-bonsu25 <pbsosei@st.ug.edu.gh>
2026-07-29 10:55:50 +0000

    Implement prime checking function

--- assignment_01_prime_checker.cpp
+++ assignment_01_prime_checker.cpp
@@ -35,5 +35,33 @@
 // =============================================================================
 
 #include <iostream>
+#include <cmath>
 using namespace std;
+
+bool isPrime(int number) {
+    if (number < 2) {
+        return false;
+    }
+    for (int i = 2; i <= sqrt(number); i++) {
+        if (number % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    if (isPrime(num)) {
+        cout << num << " is a prime number." << endl;
+    } else {
+        cout << num << " is NOT a prime number." << endl;
+    }
+    return 0;
+}
+
+
+
 
